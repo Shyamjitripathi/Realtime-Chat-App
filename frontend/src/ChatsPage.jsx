29d6aa76ad9f87874324b4b05990ba20e9329406
shyamjitripathi <shyamjitripathi2002@gmail.com>
2023-04-22 18:30:25 +0530

    improving chatpage

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,15 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div className="background">
+      <PrettyChatWindow
+        projectId='a564ef3e-d6b6-44b1-8c8e-75596f780eba'
+        username={props.user.username}
+        secret={props.user.secret}
+      />
+    </div>
+  )
 }
 
 export default ChatsPage;
